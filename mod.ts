4995f7f860379a53b233a7a3b240102f41539eaa
oliver <kookooloopdaking@gmail.com>
2022-08-11 21:50:37 -0500

    Add index page

--- mod.ts
+++ mod.ts
@@ -1,14 +1,25 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-serve(async (req, inf) => {
-    return new Response(
-        await Deno.readTextFile(
-            "./genders" + req.url.substring(req.url.indexOf("/",8)) + ".json"
-        ),
-        {
-            "headers": {
-                "content-type": "text/json"
-            }
+async function readAllInDir(pathname: string, addendum?: string): Promise<string[]> {
+    const out: string[] = []
+    const files = Deno.readDir(pathname)
+    for await (const file of files) {
+        if (file.isDirectory) {
+            const newAddendum = addendum? file.name+"/"+addendum: file.name
+            out.push(...await readAllInDir(pathname + "/" + file.name, newAddendum))
+            continue
         }
-    )
+        const name = file.name.substring(0, file.name.lastIndexOf("."))
+        out.push("/" + (addendum? addendum + "/": "") + name)
+    }
+    return out
+}
+
+serve(async (req,) => {
+    const loc = new URL(req.url).pathname;
+    if (loc == "/") {
+        const out = await readAllInDir("./genders");
+        return Response.json(out)
+    }
+    return Response.json(JSON.parse(await Deno.readTextFile("./genders" + loc + ".json")))
 })
