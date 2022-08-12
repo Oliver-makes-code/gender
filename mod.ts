722dff41fe9414ea8dfb0df0d4f8e67a105b92de
oliver <kookooloopdaking@gmail.com>
2022-08-11 20:34:30 -0500

    Fix response

--- mod.ts
+++ mod.ts
@@ -1,9 +1,14 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-serve(async (req) => {
-    return Response.json(
+serve(async (req, inf) => {
+    return new Response(
         await Deno.readTextFile(
             "./genders" + req.url.substring(req.url.indexOf("/",8)) + ".json"
-        )
+        ),
+        {
+            "headers": {
+                "content-type": "text/json"
+            }
+        }
     )
 })
